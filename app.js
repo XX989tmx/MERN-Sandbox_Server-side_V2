a083a83d2a5c9099ab4e87adb7a1a69efe026b7a
XX989tmx <issacgravity8@gmail.com>
2020-06-24 22:07:01 +0900

    article routes

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
+
 const articlesRoutes = require('./routes/articles-routes');
 
 const app = express();
 
 app.use('/api/articles', articlesRoutes);
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error)
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occurred!'});
+});
 
 app.listen(5000);
