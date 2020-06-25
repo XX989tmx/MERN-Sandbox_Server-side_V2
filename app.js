88492454a2aab951d6775c77346632863f23976e
XX989tmx <issacgravity8@gmail.com>
2020-06-25 21:44:12 +0900

    signup login routes

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser');
 const HttpError = require('./models/http-error');
 
 const articlesRoutes = require('./routes/articles-routes');
+const usersRoutes = require('./routes/users-routes');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api/articles', articlesRoutes);
+
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
     const error = new HttpError('Could not find this route.', 404);
