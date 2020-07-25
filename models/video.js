dc8a518eb4c7239afdedbea856eac8b29fd406ba
XX989tmx <issacgravity8@gmail.com>
2020-07-26 06:05:44 +0900

    modified videojs

--- models/video.js
+++ models/video.js
@@ -5,11 +5,15 @@ const Schema = mongoose.Schema;
 const videoSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  person: { type: String, required: true },
-  url: { type: String, required: true },
-  admin: { type: mongoose.Types.ObjectId, required: true, ref: "Admin" },
-  likes: [{ type: mongoose.Types.ObjectId, required: true, ref: "Like" }],
-  dislikes: [{ type: mongoose.Types.ObjectId, required: true, ref: "Dislike" }],
+  persons: [{ type: String, required: true }],
+  src: { type: String, required: true },
+  tags: [{ type: String, required: true }],
+  categories: [{ type: String, required: true }],
+  genre: [{ type: String, required: true }],
+  date_created: { type: String, required: true },
+  // admin: { type: mongoose.Types.ObjectId, required: true, ref: "Admin" },
+  // likes: [{ type: mongoose.Types.ObjectId, required: true, ref: "Like" }],
+  // dislikes: [{ type: mongoose.Types.ObjectId, required: true, ref: "Dislike" }],
 });
 
 module.exports = mongoose.model("Video", videoSchema);
