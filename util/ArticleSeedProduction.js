e0e7cd573f539cbbd36d107c6a6f27fff02eed52
XX989tmx <issacgravity8@gmail.com>
2020-08-20 11:02:04 +0900

    adds sample seedData and comment it out. if had problem, check this commit

---
+++ util/ArticleSeedProduction.js
@@ -0,0 +1,69 @@
+// db.articles.insert({
+//   wishlists: [],
+//   categories: ["investment"],
+//   tags: ["investment"],
+//   author: ObjectId("5f3c6e050f1173001719d583"),
+//   title: "dCompletely strategize",
+//   content: "sample",
+//   address: "kiyomizudera",
+//   location: { lat: 34.9948561, lng: 135.7850463 },
+//   image:
+//     "https://storage.cloud.google.com/sample_test_image_bucket/images/download-2.jpg",
+//   date_created: new Date(Date.now()).toString(),
+//   price: Math.floor(Math.random() * (12000 - 287) + 287),
+// });
+
+// var bulk = db.articles.initializeUnorderedBulkOp();
+// bulk.insert({
+//   wishlists: [],
+//   categories: ["investment"],
+//   tags: ["business"],
+//   author: ObjectId("5f3c6e050f1173001719d583"),
+//   title: "Distinctively transition proactive markets",
+//   content: "sample",
+//   address: "kiyomizudera",
+//   location: { lat: 34.9948561, lng: 135.7850463 },
+//   image: "uploads/images/a4d7c862-00bc-4cfb-9ff5-59c1d7033dc2.jpeg",
+//   date_created: new Date(Date.now()).toString(),
+//   price: Math.floor(Math.random() * (12000 - 287) + 287),
+// });
+// bulk.insert({
+//   wishlists: [],
+//   categories: ["investment"],
+//   tags: ["business"],
+//   author: ObjectId("5f3c6e050f1173001719d583"),
+//   title: "Authoritatively leverage existing team building",
+//   content: "sample",
+//   address: "kiyomizudera",
+//   location: { lat: 34.9948561, lng: 135.7850463 },
+//   image: "uploads/images/a4d7c862-00bc-4cfb-9ff5-59c1d7033dc2.jpeg",
+//   date_created: new Date(Date.now()).toString(),
+//   price: Math.floor(Math.random() * (12000 - 287) + 287),
+// });
+// bulk.insert({
+//   wishlists: [],
+//   categories: ["society"],
+//   tags: ["society"],
+//   author: ObjectId("5f3c6e050f1173001719d583"),
+//   title: "Seamlessly orchestrate premium growth strategies",
+//   content: "sample",
+//   address: "kiyomizudera",
+//   location: { lat: 34.9948561, lng: 135.7850463 },
+//   image: "uploads/images/a4d7c862-00bc-4cfb-9ff5-59c1d7033dc2.jpeg",
+//   date_created: new Date(Date.now()).toString(),
+//   price: Math.floor(Math.random() * (12000 - 287) + 287),
+// });
+// bulk.insert({
+//   wishlists: [],
+//   categories: ["business"],
+//   tags: ["investment"],
+//   author: ObjectId("5f3c6e050f1173001719d583"),
+//   title: "Dramatically conceptualize cross-media leadership",
+//   content: "sample",
+//   address: "kiyomizudera",
+//   location: { lat: 34.9948561, lng: 135.7850463 },
+//   image: "uploads/images/a4d7c862-00bc-4cfb-9ff5-59c1d7033dc2.jpeg",
+//   date_created: new Date(Date.now()).toString(),
+//   price: Math.floor(Math.random() * (12000 - 287) + 287),
+// });
+// bulk.execute();
